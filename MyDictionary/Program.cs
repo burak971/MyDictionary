e0700cba4c3c32b495691e836757747533843c02
burak karapınar <b.burak1997@hotmail.com>
2021-01-31 11:44:05 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> FenerbahceTakımıOyuncuları = new MyDictionary<int, string>();
+
+           
+            FenerbahceTakımıOyuncuları.Add(1, "ismail");
+            FenerbahceTakımıOyuncuları.Add(1, "Osayi");
+            FenerbahceTakımıOyuncuları.Add(1, "Szalai");
+            FenerbahceTakımıOyuncuları.Add(1, "Mesut");
+        }
+    }
+}
